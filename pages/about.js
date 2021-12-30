8881888fae3a62c66da3056438399b5d70fcaa72
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2021-12-29 20:26:36 -0500

    testing next router

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const about = () => {
     return (
@@ -6,6 +7,7 @@ const about = () => {
             <h1>About Page</h1>
             <p>This is a site to plan events</p>
             <p>V.1.0</p>
+            <Link href="/">Home Page</Link>
         </div>
     );
 };
