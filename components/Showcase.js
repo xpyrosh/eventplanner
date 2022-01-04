b7b4089a2af58eb04073296411aaa235f968cc3a
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2022-01-03 22:53:58 -0500

    added showcase component to layout (only to show on homepage with next/router) as well as images files for project

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from "@/styles/Showcase.module.css";
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+            <h1>Discover Toronto!</h1>
+            <h2>Find the hottest events!</h2>
+        </div>
+    );
+}
